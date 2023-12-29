dfa5d1c2a1830c2d04247f861cee3f422bc60002
123 <mac@Macs-Mac-mini.local>
2023-12-29 15:29:04 +0200

    123

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  
     images: {
         domains: ['giphy.com', 'media0.giphy.com', 'media2.giphy.com', 'media4.giphy.com'], 
       },
